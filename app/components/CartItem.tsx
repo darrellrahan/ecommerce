da5ed43cd80e6684b8383ee94e5b396faf3d50c9
Darrell-Rafa <drafa0609@gmail.com>
2023-04-28 11:50:54 +0700

    cart new design

--- app/components/CartItem.tsx
+++ app/components/CartItem.tsx
@@ -4,8 +4,8 @@ import { BsTrash } from "react-icons/bs";
 
 function CartItem() {
   return (
-    <div className="border border-black w-60 m-auto">
-      <div>
+    <div className="border border-black flex">
+      <div className="basis-1/4 flex items-center justify-center">
         <Image
           src="/images/products/furniture/shelf-black/display.jpg"
           alt="img"
@@ -13,26 +13,32 @@ function CartItem() {
           height={1000}
         />
       </div>
-      <div className="p-4 bg-[#e5e5e5] space-y-6">
-        <div>
-          <h1 className="text-xl">Gejst Shelf A Black Ash/Black</h1>
+      <div className="basis-3/4 p-4 bg-[#e5e5e5] flex justify-between gap-8">
+        <div className="flex flex-col gap-4 justify-between">
+          <div>
+            <h1 className="text-xl">Gejst Shelf A Black Ash/Black</h1>
+          </div>
+          <div className="flex">
+            <button className="bg-white w-6 h-6 flex items-center justify-center border-t border-l border-b border-black hover:bg-transparent transition-all duration-300 ease-linear text-xl">
+              -
+            </button>
+            <p className="w-6 h-6 flex items-center justify-center border border-black text-center text-lg">
+              1
+            </p>
+            <button className="bg-white w-6 h-6 flex items-center justify-center border-t border-r border-b border-black hover:bg-transparent transition-all duration-300 ease-linear text-xl">
+              +
+            </button>
+          </div>
         </div>
-        <div className="flex justify-center">
-          <button className="bg-white w-8 h-8 flex items-center justify-center border-t border-l border-b border-black hover:bg-transparent transition-all duration-300 ease-linear text-2xl">
-            -
-          </button>
-          <p className="w-8 h-8 flex items-center justify-center border border-black text-center text-xl">
-            1
-          </p>
-          <button className="bg-white w-8 h-8 flex items-center justify-center border-t border-r border-b border-black hover:bg-transparent transition-all duration-300 ease-linear text-2xl">
-            +
-          </button>
-        </div>
-        <div className="flex items-center justify-between">
-          <h1 className="text-xl font-semibold">$115</h1>
-          <button className="text-2xl">
-            <BsTrash />
-          </button>
+        <div className="flex flex-col items-end justify-between">
+          <div>
+            <h1 className="text-xl font-semibold">$115</h1>
+          </div>
+          <div>
+            <button className="text-2xl">
+              <BsTrash />
+            </button>
+          </div>
         </div>
       </div>
     </div>
